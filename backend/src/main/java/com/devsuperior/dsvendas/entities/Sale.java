2fca047cd5f3799a27c1148db863c023a18b94a9
Tobias <tobiasb29@gmail.com>
2021-09-08 23:04:55 -0300

    domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dsvendas.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sales")
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+	
+	public Sale() {}
+	
+	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public Seller getSeller() {
+		return seller;
+	}
+
+}
